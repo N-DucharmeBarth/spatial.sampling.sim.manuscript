28ed28e5996d5941896f159c3f73008c25a861be
N-DucharmeBarth <nicholasd@spc.int>
2020-02-03 14:58:18 +1100

    fn.plot.sim.results.summary.r: add time series plotting

--- R/Fn/fn.plot.sim.results.summary.r
+++ R/Fn/fn.plot.sim.results.summary.r
@@ -7,6 +7,9 @@
 #' Plot time series by Region & Catchability. Data subsetted by Model & Scenario
 #' @param metric.df metric.df created by parse.output.r
 #' @param ts.df ts.df created by parse.output.r
+#' @param nominal.df bring in the nominal index
+#' @param simple.true.index Bring in the true index
+#' @param plot.type For time series plots: 1 = Model.Region & 2 = Model.Scenario
 #' @param r Subset data to only this Region
 #' @param q Subset data to only these Catchability scenarios
 #' @param m Subset data to only this Model
@@ -15,6 +18,8 @@
 #' @param Save.Dir Path to directory where output is saved if Save == TRUE
 #' @import ggplot2
 #' @import ggthemes
+#' @importFrom scales alpha
+#' @import data.table
 #' @export
 
 # load packages
@@ -155,5 +160,62 @@
   			dpi = 300, limitsize = TRUE)
 		} else {
 			p
+		}
+	}
+
+	plot.ts = function(ts.df,nominal.df,simple.true.index,plot.type,r,s,q,m,Save,Save.Dir)
+	{
+		if(plot.type == 1)
+		{
+			tmp.df = na.omit(subset(ts.df,Region==r&Catchability==q&Model==m&mgc<=1e-04))
+			tmp.dt = data.table::as.data.table(tmp.df)
+			tmp.dt = tmp.dt[,.(Index = median(Index),Index.10 = quantile(Index,probs=0.1),Index.90 = quantile(Index,probs=0.9)),by=.(Scenario,Catchability,Model,Region,Year)]
+			tmp.dt$Model = "Estimated"
+			true.df = data.frame(Year=seq(from=1979,length.out=120,by=0.25),True=simple.true.index[,r])
+			true.df$Model = "True"
+
+			nom.df = na.omit(subset(nominal.df,Region==r&Catchability==q&Model=="Nominal"))
+			nom.dt = data.table::as.data.table(nom.df)
+			nom.dt = nom.dt[,.(Index = median(Index),Index.10 = quantile(Index,probs=0.1),Index.90 = quantile(Index,probs=0.9)),by=.(Scenario,Catchability,Model,Region,Year)]
+
+			tmp.dt = rbind(tmp.dt,nom.dt)
+			
+			p = ggplot(data = tmp.dt, aes(x=Year, y=Index,color=Model,fill=Model)) + facet_wrap(~Scenario) + ggtitle(paste0("Region: ",r," - Model: ",m)) +
+				geom_ribbon( aes(x=Year, ymin=Index.10,ymax=Index.90,color=NA)) + geom_line(aes(x=Year,y=Index)) + geom_point(data = true.df,aes(x=Year,y=True)) +
+			    theme_few() + scale_fill_manual(values=scales::alpha(c("#03a9f4","gray70","black"),0.5),name = "Index Type") + scale_color_manual(values = c("#0277bd","gray30","black"),name = "Index Type")
+			if(Save)
+			{
+				ggsave(filename=paste0("ts.",m,".",r,".png"), plot = p, device = "png", path = Save.Dir,
+	  			scale = 1.25, width = 9, height = 6, units = c("in"),
+	  			dpi = 300, limitsize = TRUE)
+			} else {
+				p
+			}
+		} else {
+			tmp.df = na.omit(subset(ts.df,Scenario==s&Catchability==q&Model==m&mgc<=1e-04))
+			tmp.dt = data.table::as.data.table(tmp.df)
+			tmp.dt = tmp.dt[,.(Index = median(Index),Index.10 = quantile(Index,probs=0.1),Index.90 = quantile(Index,probs=0.9)),by=.(Scenario,Catchability,Model,Region,Year)]
+			tmp.dt$Model = "Estimated"
+			
+			true.df = data.frame(Year=rep(seq(from=1979,length.out=120,by=0.25),9),True=do.call(c,lapply(1:9,function(x)simple.true.index[,x])),Region=do.call(c,lapply(c("all",1:8),function(x)rep(x,120))))
+			true.df$Model = "True"
+
+			nom.df = na.omit(subset(nominal.df,Scenario==s&Catchability==q&Model=="Nominal"))
+			nom.dt = data.table::as.data.table(nom.df)
+			nom.dt = nom.dt[,.(Index = median(Index),Index.10 = quantile(Index,probs=0.1),Index.90 = quantile(Index,probs=0.9)),by=.(Scenario,Catchability,Model,Region,Year)]
+
+			tmp.dt = rbind(tmp.dt,nom.dt)
+			
+			p = ggplot(data = tmp.dt, aes(x=Year, y=Index,color=Model,fill=Model)) + facet_wrap(~Region) + ggtitle(paste0("Effort scenario: ",s," - Model: ",m)) +
+				geom_ribbon( aes(x=Year, ymin=Index.10,ymax=Index.90,color=NA)) + geom_line(aes(x=Year,y=Index)) + geom_point(data = true.df,aes(x=Year,y=True)) +
+			    theme_few() + scale_fill_manual(values=scales::alpha(c("#03a9f4","gray70","black"),0.5),name = "Index Type") + scale_color_manual(values = c("#0277bd","gray30","black"),name = "Index Type")
+			if(Save)
+			{
+				ggsave(filename=paste0("ts.",m,".",s,".png"), plot = p, device = "png", path = Save.Dir,
+	  			scale = 1.25, width = 9, height = 6, units = c("in"),
+	  			dpi = 300, limitsize = TRUE)
+			} else {
+				p
+			}
 		}
 	}
